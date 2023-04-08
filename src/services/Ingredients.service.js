c4a092e7413bd895fbcea85e87b8955048895246
Vadym <the.rocky.vadim@gmail.com>
2023-04-08 16:25:23 +0300

    add api base services

---
+++ src/services/Ingredients.service.js
@@ -0,0 +1,14 @@
+import config from "../../config";
+
+const ingredientsEndpoint = 'https://api.edamam.com/auto-complete'
+
+export async function fetchIngredients(query) {
+    const response = await fetch(`${ingredientsEndpoint}?q=${query}&app_id=${config.edamamAppId}&app_key=${config.edamamAppKey}`);
+
+    if (response.status !== 200) {
+        return [];
+    }
+
+    const data = await response.json()
+    return Array.from(data)
+}
---
+++ src/services/RecipeData.service.js
@@ -0,0 +1,45 @@
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import firebase from "firebase/compat";
+
+export async function createRecipe(ingredients) {
+    const functions = getFunctions(firebase.default.apps[0]);
+
+    try {
+        const createRecipeFn = httpsCallable(functions, 'createRecipe');
+        const result = await createRecipeFn(ingredients);
+        return result.data;
+    } catch (error) {
+        console.error('Error calling function:', error);
+    }
+}
+
+export async function fetchUserRecipes() {
+    const store = getFirestore(firebase.default.apps[0]);
+    const auth = getAuth(firebase.default.apps[0]);
+
+    const userId = auth.currentUser.uid;
+
+    const recipesRef = store.collection('recipes');
+    const query = recipesRef.where('userId', '==', userId);
+
+    try {
+        const querySnapshot = await query.get();
+        const recipes = [];
+
+        //firebase's forEach
+        querySnapshot.forEach(doc => {
+            const recipe = {
+                id: doc.id,
+                ...doc.data()
+            };
+
+            recipes.push(recipe);
+        });
+        return recipes;
+    } catch (e) {
+        console.error('Error fetching recipes for user', userId, ':', e);
+    }
+
+}
